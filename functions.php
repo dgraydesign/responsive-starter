6971249c44f988e81a7191c1764ff3f74e5cf57d
Debbie Gray <dgray@deborahgraydesign.com>
2014-03-28 15:43:53 -0700

    Change widget titles from h1 to h2

--- functions.php
+++ functions.php
@@ -74,8 +74,8 @@ function dgraystarter_widgets_init() {
 		'id'            => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
 	) );
 }
 add_action( 'widgets_init', 'dgraystarter_widgets_init' );
