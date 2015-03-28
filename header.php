469964cee25420e984742ff1753c44e9b94f0ca2
Debbie Gray <dgray@deborahgraydesign.com>
2015-03-28 09:51:13 -0700

    improve mobile navigation

--- header.php
+++ header.php
@@ -29,10 +29,8 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><span class="dashicons dashicons-menu"></span> <?php _e( 'Menu', 'dgraystarter' ); ?></h1>
-			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'dgraystarter' ); ?></a>
-
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', '_s' ); ?></button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 		</div><!-- end site branding -->
 	</header><!-- #masthead -->
