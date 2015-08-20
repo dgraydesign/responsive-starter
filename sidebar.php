392d8c8a8f6256f53a76156934b75ca2ee2aa01d
Debbie Gray <dgray@deborahgraydesign.com>
2015-08-20 11:09:41 -0700

    update sidebar
    Removes all default sidebar widgets except for Search, and adds
    conditional display of sidebar-1

--- sidebar.php
+++ sidebar.php
@@ -6,26 +6,16 @@
  */
 ?>
 	<div id="secondary" class="c4 end widget-area" role="complementary">
+        <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+			<div id="widget-area" class="widget-area" role="complementary">
+				<?php dynamic_sidebar( 'sidebar-1' ); ?>
+			</div><!-- .widget-area -->
+		<?php endif; ?>
+        
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
 			<aside id="search" class="widget widget_search">
 				<?php get_search_form(); ?>
-			</aside>
-
-			<aside id="archives" class="widget">
-				<h1 class="widget-title"><?php _e( 'Archives', 'dgraystarter' ); ?></h1>
-				<ul>
-					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-				</ul>
-			</aside>
-
-			<aside id="meta" class="widget">
-				<h1 class="widget-title"><?php _e( 'Meta', 'dgraystarter' ); ?></h1>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
 			</aside>
 
 		<?php endif; // end sidebar widget area ?>
