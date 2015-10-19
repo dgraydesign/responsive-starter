22f8ae51330332d8983d42fcedd5781f425b3aba
Debbie Gray <dgray@deborahgraydesign.com>
2015-10-19 16:01:46 -0700

    remove prompt to bookmark permalink

--- content-single.php
+++ content-single.php
@@ -34,17 +34,15 @@
 			if ( ! dgraystarter_categorized_blog() ) {
 				// This blog only has 1 category so we just need to worry about tags in the meta text
 				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'dgraystarter' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'dgraystarter' );
+					$meta_text = __( 'This entry was tagged %2$s.', 'dgraystarter' );
 				}
 
 			} else {
 				// But this blog has loads of categories so we should probably display them here
 				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'dgraystarter' );
+					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s.', 'dgraystarter' );
 				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'dgraystarter' );
+					$meta_text = __( 'This entry was posted in %1$s.', 'dgraystarter' );
 				}
 
 			} // end check for categories on this blog
