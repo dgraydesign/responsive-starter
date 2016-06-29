2223796de9ed411a513f08d561b846c2d1b54007
Debbie Gray <dgray@deborahgraydesign.com>
2016-06-29 11:22:28 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -100,7 +100,6 @@ add_action( 'wp_enqueue_scripts', 'dgraystarter_scripts' );
 function dg_google_fonts() {
 	$query_args = array(
 		'family' => 'Open+Sans:400,700|Oswald:700'
-		'subset' => 'latin,latin-ext',
 	);
 	wp_register_style( 'dg_google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
             }
